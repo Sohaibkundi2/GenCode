65227001e681135aee90370ce529ac938bf53cb0
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-08-25 22:34:21 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className='bg-green-500 text-2xl'>Home Page</div>
+      <Button>Hellow there.....</Button>
+    </>
+  )
+}
+
+export default Home
